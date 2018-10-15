b7951795b5c88111616ac56e0338865a1a492c0e
Marcus Nielsen <marcus.nielsen82@gmail.com>
2018-10-15 14:22:07 +0200

    WIP lerna: db unit tests

--- packages/drivers-db/src/types.ts
+++ packages/drivers-db/src/types.ts
@@ -1,15 +1,19 @@
-import { IConfig } from '@karmalicious/drivers-config'
-
-export type close = () => void
-
-export type driver = (
-  sql: string,
-  options: { type: string; bind: any }
-) => Promise<any>
-
-export interface IDb {
-  close: close
-  driver: driver
+export interface IDbConfig {
+  DB_DB: string
+  DB_USR: string
+  DB_PSW: string
+  DB_HOST: string
+  SERVER_CA: string
+  CLIENT_CERT: string
+  CLIENT_KEY: string
 }
 
-export type makeDb = (c: IConfig) => IDb
+export interface ISequelizeMock {
+  close(): Promise<void>
+  query(sql: string, options: { type: string; bind: any }): Promise<any>
+}
+
+export interface IDb {
+  close(): Promise<void>
+  exec<T>(sql: string, options: { type: string; bind: any }): Promise<T>
+}
